14933c52b2c5f61b6a05d7e58b666a627cbc05b4
dhanush-source <dhanush@oneassure.in>
2025-10-17 01:23:40 +0530

    chores: fixed an import issue

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { ChatAPI } from './api/api.js';
 import { markdownToHtml, formatText, sanitizeHtml } from './utils/markdown.js';
 
 // Export main classes and utilities
-export { ChatbotWidget, chatApi, markdownToHtml, formatText, sanitizeHtml };
+export { ChatbotWidget, ChatAPI, markdownToHtml, formatText, sanitizeHtml };
 
 // Default export for convenience
 export default ChatbotWidget;
